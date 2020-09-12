3373e56e87dd9e5cb44e1d04524557cffe198c78
friosl <friosl@eafit.edu.co>
2020-09-12 15:58:04 -0500

    Pull request
    Subiendo archivos de la clase meeting

---
+++ PetShop/app/Meeting.php
@@ -0,0 +1,54 @@
+<?php
+//Autor : Felipe Ríos López
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+class Meeting extends Model
+{
+    //attributes id, name, price, created_at, updated_at
+    protected $fillable = ['place', 'date','details'];
+    public function getId()
+    {
+        return $this->attributes['id'];
+    }
+
+    public function setId($id)
+    {
+        $this->attributes['id'] = $id;
+    }
+
+    public function getDate()
+    {
+        return $this->attributes['date'];
+    }
+
+    public function setDate($date)
+
+    {
+        $this->attributes['date'] = $date;
+    }
+
+    public function getPlace()
+    {
+        return $this->attributes['place'];
+    }
+
+    public function setPlace($place)
+    {
+        $this->attributes['place'] = $place;
+    }
+    public function getDetails()
+    {
+        return $this->attributes['details'];
+    }
+
+    public function setDetails($details)
+    {
+        $this->attributes['details'] = $details;
+    }
+
+    public function user() 
+    {
+        return $this->belongsToMany('App\User');
+    }
+    
+}
